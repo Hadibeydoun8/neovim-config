70a2a625c7dfd8abed55c00c36c6676cefad5fac
hadibeydoun <76006521+Hadibeydoun8@users.noreply.github.com>
2026-04-03 18:34:37 -0400

    Add copiloit to config
    Updated nvim cmp to accept copiloit sugesstions
    Added copiloit.lua file for github copiloit supourt

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -22,6 +22,7 @@ return {
 				["<S-Tab>"] = cmp.mapping.select_prev_item(),
 			}),
 			sources = {
+				{ name = "copilot" },
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "buffer" },
