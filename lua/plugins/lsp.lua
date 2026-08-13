a27e4fb9b340a935daa3d093c037194800854206
Hadi <hbeydoun@mdanderson.org>
2026-08-12 20:25:52 -0500

    Add swift LSP and reformat file

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,33 +1,47 @@
 return {
-	{
-		"mason-org/mason-lspconfig.nvim",
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"ts_ls",
-					"clangd",
-					"rust_analyzer",
-					"omnisharp",
-					"lua_ls",
-				},
-			}) -- <-- this was missing
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "ts_ls",
+                    "clangd",
+                    "rust_analyzer",
+                    "omnisharp",
+                    "lua_ls",
+                },
+            }) -- <-- this was missing
 
-			vim.lsp.config("*", {
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-			})
+            vim.lsp.config("*", {
+                capabilities = require("cmp_nvim_lsp").default_capabilities(),
+            })
 
-			vim.lsp.enable({
-				"ts_ls",
-				"clangd",
-				"rust_analyzer",
-				"omnisharp",
-				"lua_ls",
-			})
-		end,
-	},
+            vim.lsp.enable({
+                "ts_ls",
+                "clangd",
+                "rust_analyzer",
+                "omnisharp",
+                "lua_ls",
+            })
+
+            -- Use Apple's SourceKit-LSP from the active Xcode toolchain.
+            vim.lsp.config("sourcekit", {
+                cmd = { "xcrun", "sourcekit-lsp" },
+            })
+
+            vim.lsp.enable({
+                "ts_ls",
+                "clangd",
+                "rust_analyzer",
+                "omnisharp",
+                "lua_ls",
+                "sourcekit",
+            })
+        end,
+    },
 }
