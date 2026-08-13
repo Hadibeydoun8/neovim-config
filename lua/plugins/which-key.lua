817bb350634df61a8286485e627f3043936f4772
Hadi <hbeydoun@mdanderson.org>
2026-08-12 20:17:52 -0500

    Add decs for whichkey, and make which key look better

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,13 +2,27 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-        delay = 500, -- ms delay before popup appears
+        preset = "modern",
+        delay = 300, -- ms delay before popup appears
+        spec = {
+            { "<leader>f", group = "Find (Telescope)" },
+            { "<leader>d", group = "Database" },
+            { "<leader>o", group = "Overseer (tasks)" },
+            { "<leader>g", group = "Git" },
+            { "<leader>x", group = "Trouble/Diagnostics" },
+            { "<leader>c", group = "Code" },
+        },
     },
     keys = {
         {
             "<leader>?",
             function() require("which-key").show({ global = false }) end,
             desc = "Buffer keymaps (which-key)",
+        },
+        {
+            "<leader>k",
+            function() require("which-key").show({ global = true }) end,
+            desc = "All keymaps (which-key)",
         },
     },
 }
