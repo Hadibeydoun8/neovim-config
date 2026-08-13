1723ba66cb4535974c29610b4c98892413a7933f
Hadi <hbeydoun@mdanderson.org>
2026-08-12 19:55:39 -0500

    Explorer Change
    Updated explorer to use snacks rather than the built in one

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,48 +1,65 @@
 -- lua/plugins/snacks.lua
+-- lua/plugins/snacks.lua
 return {
-	"folke/snacks.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = {
-		bigfile = { enabled = true }, -- auto-disable LSP/treesitter on huge files
-		notifier = { enabled = true }, -- better vim.notify
-		terminal = { enabled = true }, -- toggle terminal
-		lazygit = { enabled = true }, -- lazygit integration
-		scratch = { enabled = true }, -- quick scratch buffers
-		-- leave these off since you already have plugins for them:
-		dashboard = { enabled = false },
-		indent = { enabled = false },
-		explorer = { enabled = false },
-		picker = { enabled = false },
-	},
-	keys = {
-		{
-			"<leader>gg",
-			function()
-				Snacks.lazygit()
-			end,
-			desc = "Lazygit",
-		},
-		{
-			"<c-/>",
-			function()
-				Snacks.terminal()
-			end,
-			desc = "Toggle Terminal",
-		},
-		{
-			"<leader>.",
-			function()
-				Snacks.scratch()
-			end,
-			desc = "Scratch Buffer",
-		},
-		{
-			"<leader>un",
-			function()
-				Snacks.notifier.hide()
-			end,
-			desc = "Dismiss Notifications",
-		},
-	},
+    "folke/snacks.nvim",
+    lazy = false,
+    priority = 1000,
+
+    opts = {
+        bigfile = { enabled = true },
+        notifier = { enabled = true },
+        terminal = { enabled = true },
+        lazygit = { enabled = true },
+        scratch = { enabled = true },
+
+        dashboard = { enabled = false },
+        indent = { enabled = false },
+
+        explorer = {
+            enabled = true,
+            replace_netrw = true,
+        },
+
+        picker = {
+            enabled = true,
+        },
+    },
+
+    keys = {
+        {
+            "<leader>e",
+            function()
+                Snacks.explorer()
+            end,
+            desc = "File Explorer",
+        },
+        {
+            "<leader>gg",
+            function()
+                Snacks.lazygit()
+            end,
+            desc = "Lazygit",
+        },
+        {
+            "<c-/>",
+            function()
+                Snacks.terminal()
+            end,
+            desc = "Toggle Terminal",
+        },
+        {
+            "<leader>.",
+            function()
+                Snacks.scratch()
+            end,
+            desc = "Scratch Buffer",
+        },
+        {
+            "<leader>un",
+            function()
+                Snacks.notifier.hide()
+            end,
+            desc = "Dismiss Notifications",
+        },
+    },
 }
